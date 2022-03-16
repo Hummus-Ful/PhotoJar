24676656038bf9ba9ce552043fcf1c00bd1ce50f
Hummus-Ful <11685796+Hummus-Ful@users.noreply.github.com>
2022-03-16 22:39:29 +0200

    add main function

--- src/main/java/PhotoJar.java
+++ src/main/java/PhotoJar.java
@@ -1,5 +1,31 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PhotoJar {
 
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) throw new IllegalArgumentException();
+        String rootDir = args[0];
+
+        Scan scan = new Scan(rootDir);
+        ArrayList<Photo> photos = scan.getAllPhotos();
+
+        Duplicate duplicate = new Duplicate();
+        Unique unique = new Unique();
+
+        for (Photo photo : photos) {
+            if (unique.isExists(photo.getHash())) {
+                System.out.println("Duplicated hash:" + photo.getHash());
+                duplicate.add(photo);
+            }
+            else unique.add(photo);
+        }
+
+        ArrayList<Photo> duplicatedPhotos = duplicate.getAll();
+        //TODO: move all dups to a seperated folder
+    }
+
+}
     //TODO:
     /*
     1. Create new Scan object called scan
@@ -10,4 +36,4 @@ public class PhotoJar {
         b. else insert into unique
     5. Move all dups into recycleBin folder
     */
-}
+
