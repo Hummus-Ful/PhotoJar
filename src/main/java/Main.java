30ee4a119f546b0bc4704fc67e939a0fb774b6d6
Hummus-Ful <11685796+Hummus-Ful@users.noreply.github.com>
2022-04-16 22:35:41 +0300

    allow more than one directory as app args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.StringWriter;
 import java.io.Writer;
 import java.io.FileWriter;
 import java.util.Map;
@@ -18,7 +19,7 @@ public class Main {
 
     private static final String logFileName = "photojar.log";
     private static Logger logger;
-    private static String rootDir;
+    private static String[] dirs;
     private static ArrayList<Photo> photos;
     private static PhotoJar photoJar;
     private static HashMap<String, String> similar;
@@ -30,7 +31,7 @@ public class Main {
         try {
             handler = new FileHandler(logFileName);
             handler.setFormatter(new SimpleFormatter());
-            logger.setUseParentHandlers(false);  // disable console output
+            logger.setUseParentHandlers(false);  // disable outputting log to console
             logger.addHandler(handler);
         }
         catch (Exception e) {
@@ -39,15 +40,15 @@ public class Main {
     }
 
     private static void checkArgs(String[] args) {
-        if (args.length != 1) throw new IllegalArgumentException();
-        rootDir = args[0];
+        if (args.length < 1) throw new IllegalArgumentException();
+        dirs = args;
     }
 
-    private static void getAllPhotos() {
-        Scan scan = new Scan(rootDir);
+    private static void getAllPhotos(String dir) {
+        Scan scan = new Scan(dir);
         try {
             photos = scan.getAllPhotos();
-            logger.info("FOUND TOTAL OF " + photos.size() + " PHOTOS");
+            logger.info("FOUND TOTAL OF " + photos.size() + " PHOTOS IN ROOT DIR " + dir);
         }
         catch (Exception e) {
         // TODO
@@ -55,7 +56,6 @@ public class Main {
     }
 
     private static void populatePhotoJar() {
-        photoJar = new PhotoJar();
         for (Photo photo: photos) {
             photoJar.add(photo);
         }
@@ -100,12 +100,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
         setLogger();
         checkArgs(args);
         System.out.println("Starting a scan");
-        getAllPhotos();
-        populatePhotoJar();
+        photoJar = new PhotoJar();  //setup only once for the whole app
+        for (String dir : dirs) {
+            getAllPhotos(dir);
+            populatePhotoJar();
+        }
         logAllDuplicates();
         double prePopulatedMaxDistance = 0.05;
         logAllSimilar(prePopulatedMaxDistance);
